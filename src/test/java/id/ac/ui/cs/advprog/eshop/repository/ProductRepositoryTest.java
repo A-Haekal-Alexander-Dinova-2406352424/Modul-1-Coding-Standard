5ea7179a646487c90751d687c32001daa69afbc7
dinovaslate <dinovasala3@gmail.com>
2026-02-09 21:34:38 +0700

    Add ProductRepositoryTest

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProductRepositoryTest {
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        this.productRepository = new ProductRepository();
+    }
+
+    @Test
+    void testCreateAndFind() {
+        Product product = new Product();
+        product.setProductId("eb558bf-f1c9-460e-8860-71af6af63db6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        productRepository.create(product);
+
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+        Product savedProduct = productIterator.next();
+        assertEquals(product.getProductId(), savedProduct.getProductId());
+        assertEquals(product.getProductName(), savedProduct.getProductName());
+        assertEquals(product.getProductQuantity(), savedProduct.getProductQuantity());
+    }
+}
